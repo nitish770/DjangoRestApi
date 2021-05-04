9ff73a7b9b20806a7f83bc602e9c60066c878376
nitish770 <nitishkmahto257@gmail.com>
2021-05-04 09:22:51 +0530

    Fix django.db.utils.OperationalError: no such column: core_user.is_active The problem was typo in line 38     - Changed is_alive to is_active

--- app/core/models.py
+++ app/core/models.py
@@ -28,7 +28,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_alive = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
