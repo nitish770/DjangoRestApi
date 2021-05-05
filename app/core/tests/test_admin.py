3f8d5b7850d4f8da56a4565a643fad6c15482316
nitish770 <nitishkmahto257@gmail.com>
2021-05-05 10:29:59 +0530

    Finish test_admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -3,36 +3,39 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
-class AdminSiteTests(TestCase):
+class AdminSiteTest(TestCase):
 
-	def setUp(self):
-		self.client = Client()
-		self.admin_user = get_user_model().objects.create_superuser(
-			email='admin@gmail.com',
-			password='test123'
-		)
-		self.client.force_login(self.admin_user)
-		self.user = get_user_model().objects.create_user(
-			email='user@gmail.com',
-			password='test123',
-			name='User 1'
-		)
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@gmail.com",
+            password="test123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@gmail.com',
+            password='test123',
+            name="User 1"
+        )
 
-	def test_user_listed(self):
-		"""Test For users listed in user page"""
-		url = reverse('admin:core_user_changelist')
-		res = self.client.get(url)
-		self.assertContains(res, self.user.name)
-		self.assertContains(res, self.user.email)
+    def test_user_listed(self):
+        """Test the listed users"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-	def test_user_change_page(self):
-		"""Test that user edit page works"""
-		url = reverse('admin:core_user_change', args=[self.user.id])
-		res = self.client.get(url)
-		self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
-	def test_create_user_page(self):
-		"""Test that the create user page works"""
-		url = reverse('admin:core_user_add')
-		res = self.client.get(url)
-		self.assertEqual(res.status_code, 200)
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
